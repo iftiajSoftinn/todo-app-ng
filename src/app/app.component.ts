89fd179e384124b41730157c55094091d208265f
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-07-07 05:49:42 +0800

    Todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from "./Todo";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app-ng';
+
+
+  todos : Todo[] = [];
+  newTodo : string;
+
+  saveTodo(){
+    if (this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true
+      this.todos.push(todo);
+      this.newTodo = "";
+
+
+    }
+  else{
+    alert("Please enter todo")
+  };
+  }
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+  remove(id:number){
+    this.todos = this.todos.filter((v,i)=> i !==id);
+  }
 }
